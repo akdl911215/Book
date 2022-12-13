e1640b2f975258c6421112614c7f995aafe6773a
akdl911215 <akdl911215@naver.com>
2022-12-13 22:02:33 +0900

    tip

--- 자바스크립트코딩의기술/chapter3/tip34/problem.js
+++ 자바스크립트코딩의기술/chapter3/tip34/problem.js
@@ -2,5 +2,5 @@ const partial = require('./partial.js')
 const { building, manager, exhibit, program} = partial
 
 function mergeProgramInformation (building, manager, event) {
-    // const { } = building;
+    const { } = building;
 }
