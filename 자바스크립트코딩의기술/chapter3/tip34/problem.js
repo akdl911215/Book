3179407445e6b9a7d1ea24b664c390c39f460718
akdl911215 <akdl911215@naver.com>
2022-12-19 22:30:11 +0900

    #

--- 자바스크립트코딩의기술/chapter3/tip34/problem.js
+++ 자바스크립트코딩의기술/chapter3/tip34/problem.js
@@ -1,6 +1,27 @@
-const partial = require('./partial.js')
-const { building, manager, exhibit, program} = partial
+const building = {
+    hours: '8 a.m. - 8 a.m.',
+    address: 'Jayhawk Blvd',
+};
+const manager = {
+    name: 'Augusto',
+    phone: '555-555-5555',
+};
+const program = {
+    name: 'Presenting Research',
+    room: '415',
+    hours: '3 - 6',
+}
+const exhibit = {
+    name: 'Emerging Scholarship',
+    contact: 'Dyan',
+}
 
 function mergeProgramInformation (building, manager, event) {
-    const { } = building;
+    const { hours, address } = building;
+    const { name, phone } = manager;
+    const defaults = {
+        hours, address, contact: name, phone
+    }
 }
+
+mergeProgramInformation(building, manager)
