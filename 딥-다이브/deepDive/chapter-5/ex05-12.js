4a0ea8ce328652a79c14b507fd543fe04e4b9412
akdl911215 <akdl911215@naver.com>
2023-01-15 16:29:31 +0900

    js dive study chapter 5

---
+++ 딥-다이브/deepDive/chapter-5/ex05-12.js
@@ -0,0 +1,11 @@
+
+
+// 표현식이 아닌 문은 값처럼 사용할 수 없다.
+// var bar = var x; // SyntaxError: Unexpected token 'var'
+
+// let foo = let y; // SyntaxError: Unexpected identifier
+
+
+
+
+
---
+++ 딥-다이브/deepDive/chapter-6/ex06-01.js
@@ -0,0 +1,8 @@
+
+
+// 모두 숫자 타입이다.
+const integer = 10;     // 정수
+const double = 10.12;   // 실수
+const negative = -20;   // 음의 정수
+
+
